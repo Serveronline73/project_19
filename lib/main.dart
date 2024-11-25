a5e9b349f983ca90105e1d5b2eff76f2c705cc50
dennis <info.dennis.durmus@gmail.com>
2024-11-25 22:50:07 +0100

    Added text widget, example quote, appBar background, and text color; implemented button element without functionality

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100.0),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            title: const Text("Wilkommen zu meiner\nZitat App",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center),
+          ),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Man muss das Unmögliche versuchen, um das Mögliche zu erreichen.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0, color: Colors.amber),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text("Nächstes Zitat",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
