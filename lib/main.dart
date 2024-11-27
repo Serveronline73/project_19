401c718a7454857e81139bc6ff5571a3210c8855
dennis <info.dennis.durmus@gmail.com>
2024-11-27 19:15:21 +0100

    The quotes are reloaded with each click of the button. Thanks to the integrated HTTP library, the quotes can be retrieved directly from the server.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MainApp> {
+  String quote =
+      "Es ist Unmöglich, auf zwei Hochzeiten gleichzeitig zu Tanzen.";
+  String author = "Dennis Durmus";
+
+  Future<void> getQuote() async {
+    final response = await http.get(
+      Uri.parse("https://api.api-ninjas.com/v1/quotes"), //API URL
+      headers: {
+        'X-Api-Key': 'XF+7Za8J2OnKzmCpWUvk0A==6XqWT1IqNknaRRHq'
+      }, //API Key
+    );
+
+    if (response.statusCode == 200) {
+      //Status Code 200 = OK
+      final data = json.decode(response.body); //JSON Decodieren
+      setState(() {
+        quote = data[0]['quote'];
+        author = data[0]['author'];
+      });
+    } else {
+      throw Exception("Fehler Laden vom Zitat");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +61,21 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                "Man muss das Unmögliche versuchen, um das Mögliche zu erreichen.",
+              Text(
+                quote, //Zitat
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24.0, color: Colors.amber),
+                style: const TextStyle(fontSize: 24.0, color: Colors.amber),
               ),
+              Text("- $author", //Autor
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.amber,
+                  )),
               const SizedBox(height: 20.0),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: getQuote,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.amber,
                   shape: RoundedRectangleBorder(
